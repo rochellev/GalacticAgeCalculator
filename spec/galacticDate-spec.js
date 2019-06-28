a979030934f822692536fc5fb37c1757d10e5fca
rochellev <rochellevroberts@utexas.edu>
2019-06-28 12:50:53 -0700

    added test for calculateGalacticAge()

--- spec/galacticDate-spec.js
+++ spec/galacticDate-spec.js
@@ -4,21 +4,26 @@
 import { GalacticDate } from './../src/GalacticDate.js';
 
 describe('GalacticDate', function() {
-    let mydate = new GalacticDate(2016, 5, 24, 50);
+    let mydate = new GalacticDate(2016, 5, 24, 50, "Mars");
   it('should test whether constructor works', function() {
     expect(mydate.getFullYear()).toEqual(2016);
     expect(mydate.getMonth()).toEqual(5);
     expect(mydate.getDate()).toEqual(24);
   });
 
-  it('should test whether GalacticDate methods calculate ages correctly', function(){
+  it('should test whether helper functions in GalacticDate calculate ages correctly', function(){
     expect(mydate.getMercuryAge()).toEqual(12);
     expect(mydate.getVenusAge()).toEqual(31); 
     expect(mydate.getMarsAge()).toEqual(94);
     expect(mydate.getJupiterAge()).toEqual(593);
 
 
-  })
+  });
+
+  it('should test whether call the right age function', function(){
+    expect(mydate.calculateGalacticAge()).toEqual(94);
+
+  });
 
 
 
