c96fc96fa36e5bf9f942b18dac83736642c7ddcc
rochellev <rochellevroberts@utexas.edu>
2019-06-28 12:02:49 -0700

    unit test for galactic date methods

--- spec/galacticDate-spec.js
+++ spec/galacticDate-spec.js
@@ -1,16 +1,25 @@
 // test suite for Galactic Date
 
-// Date(year month day)
+// Date(year month day age)
 import { GalacticDate } from './../src/GalacticDate.js';
 
 describe('GalacticDate', function() {
-
+    let mydate = new GalacticDate(2016, 5, 24, 50);
   it('should test whether constructor works', function() {
-    let mydate = new GalacticDate(2016, 5, 24);
     expect(mydate.getFullYear()).toEqual(2016);
     expect(mydate.getMonth()).toEqual(5);
     expect(mydate.getDate()).toEqual(24);
   });
+
+  it('should test whether GalacticDate methods calculate ages correctly', function(){
+    expect(mydate.getMercuryAge()).toEqual(12);
+    expect(mydate.getVenusAge()).toEqual(31); 
+    expect(mydate.getMarsAge()).toEqual(94);
+    expect(mydate.getJupiterAge()).toEqual(593);
+
+
+  })
+
 
 
 
