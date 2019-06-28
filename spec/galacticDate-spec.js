484ae136690f5883c1ac511e6ced3bbf49daf92c
rochellev <rochellevroberts@utexas.edu>
2019-06-28 15:33:12 -0700

    added tests for checking remaining years

--- spec/galacticDate-spec.js
+++ spec/galacticDate-spec.js
@@ -19,29 +19,35 @@ describe('GalacticDate', function () {
         expect(mydate.getMonth()).toEqual(5);
         expect(mydate.getDate()).toEqual(24);
     });
-
     it('should test whether helper functions in GalacticDate calculate ages correctly', function () {
         expect(mydate.getMercuryAge()).toEqual(12);
         expect(mydate.getVenusAge()).toEqual(31);
         expect(mydate.getMarsAge()).toEqual(94);
         expect(mydate.getJupiterAge()).toEqual(593);
     });
-
     it('should test whether calculate galactic age branches to mars', function () {
         expect(mars.planetAge).toEqual(47);
     });
-
-    it('should test whether calculate galacric age branches to venus', function () {
+    it('should test whether years left for mars returns correct number', function () {
+        expect(mars.remainPlanetYears).toEqual(119);
+    });
+    it('should test whether calculate galactic age branches to venus', function () {
         expect(venus.planetAge).toEqual(15.5);
     });
-
-    it('should test whether calculate galacric age branches to mercury', function () {
-
+    it('should test whether years left for venus returns correct number', function () {
+        expect(venus.remainPlanetYears).toEqual(40);
+    });
+    it('should test whether calculate galactic age branches to mercury', function () {
         expect(mercury.planetAge).toEqual(6);
     });
-
-    it('should test whether calculate galacric age branches to jupiter', function () {
+    it('should test whether years left for mercury returns correct number', function () {
+        expect(mercury.remainPlanetYears).toEqual(16);
+    });
+    it('should test whether calculate galactic age branches to jupiter', function () {
         expect(jupiter.planetAge).toEqual(296.5);
+    });
+    it('should test whether years left for jupiter returns correct number', function () {
+        expect(jupiter.remainPlanetYears).toEqual(748);
     });
 
 
